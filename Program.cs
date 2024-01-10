03bd2e39e6c8a889bc6948974c27edeaded618b5
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 15:49:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Atributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs Holiday = new Songs("Holiday", "Mark", "Generous", 6);
+            Songs Ghetto = new Songs("Ghetto", "Akon", "Begin", 3);
+            Songs Soweto = new Songs("Soweto", "Maseven", "RapLyf", 2);
+            Songs Truth = new Songs("Truth", "Preelicant", "Promise", 5);
+
+
+            Console.WriteLine(Songs.songCount);
+
+            Console.Read();
+
+        }
+
+        
+    }
+}
---
+++ Songs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Atributes
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public string album;
+        public int duration;
+
+        public static int songCount = 0;
+
+        public Songs(String atitle,String aartist ,String aalbum, int aduration)
+        {
+            title = atitle;
+            artist = aartist;
+            album = aalbum; 
+            duration = aduration;
+            songCount++; //increment song count
+        }
+
+        
+    }
+}
